e963f4fa9aaa062848e12b67c697c58c2bf46c02
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2023-08-29 18:19:05 -0600

    cambio en crearlog para evitar error si no hay permisos

--- src/main/java/com/imss/sivimss/arquetipo/util/LogUtil.java
+++ src/main/java/com/imss/sivimss/arquetipo/util/LogUtil.java
@@ -13,12 +13,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 @Component
 public class LogUtil {
     @Value("${ruta-log}")
     private String rutaLog;
-    
+
     @Value("${spring.application.name}")
     private String aplicacion;
 
@@ -26,28 +25,24 @@ public class LogUtil {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogUtil.class);
 
-
-    public void crearArchivoLog(String tipoLog, String origen, String clasePath, String mensaje, String tiempoEjecucion, Authentication authentication, UsuarioDto usuarioDto) throws IOException {
-        Gson json = new Gson();
-        if (usuarioDto == null ) {
-        	usuarioDto = new UsuarioDto();
-        	usuarioDto = json.fromJson((String) authentication.getPrincipal(), UsuarioDto.class);
-        }
-        File archivo = new File(rutaLog + aplicacion+ new SimpleDateFormat("ddMMyyyy").format(new Date()) + ".log");
-        FileWriter escribirArchivo = new FileWriter(archivo, true);
+    public void crearArchivoLog(String tipoLog, String origen, String clasePath, String mensaje, String tiempoEjecucion)
+            throws IOException {
+        File archivo = new File(rutaLog + aplicacion + new SimpleDateFormat("ddMMyyyy").format(new Date()) + ".log");
+        FileWriter escribirArchivo = null;
         try {
-            escribirArchivo.write("" + formatoFechaLog + " --- [" + tipoLog + "] " + origen + " " + clasePath + " : " + mensaje + " , Usuario: " + usuarioDto.getCveUsuario() + " - " + tiempoEjecucion);
+            escribirArchivo = new FileWriter(archivo, true);
+            escribirArchivo.write("" + formatoFechaLog + " --- [" + tipoLog + "] " + origen + " " + clasePath + " : "
+                    + mensaje + " ,  " + tiempoEjecucion);
             escribirArchivo.write("\r\n");
-            escribirArchivo.close();
             escribirArchivo.close();
         } catch (Exception e) {
             log.error("No se puede escribir el log.");
             log.error(e.getMessage());
-        } finally {
-            escribirArchivo.close();
+            if (escribirArchivo != null) {
+                escribirArchivo.close();
+            }
         }
 
     }
-
 
 }
